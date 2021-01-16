0096abbed6bf7c9d3357fae1bf8376fd34e8db1e
wobaidurrob <drtareqdvm014@gmail.com>
2021-01-16 16:40:31 -0500

    update

--- src/test/java/AutomationClass.java
+++ src/test/java/AutomationClass.java
@@ -4,6 +4,7 @@ public class AutomationClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("github initialization");
+		System.out.println("update the class");
 	}
 
 }
